58b78c616a1a59f21d0c56d023e206f8d471816a
JuancaOrtiz09 <juancataor@gmail.com>
2025-03-09 15:57:24 -0500

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-    let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Stiiven⁩;;\nFN:Stiiven⁩\nTITLE:\nitem1.TEL;waid=573163963499:573163963499\nitem1.X-ABLabel:JuancaOrtiz\nitem2.URL:https://wa.me/573163963499\nitem2.X-ABLabel:Enviar Mensaje\nEND:VCARD`;
+    let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;JuancaOrtiz;;\nFN:JuancaOrtiz\nTITLE:\nitem1.TEL;waid=573163963499:573163963499\nitem1.X-ABLabel:JuancaOrtiz\nitem2.URL:https://wa.me/573163963499\nitem2.X-ABLabel:Enviar Mensaje\nEND:VCARD`;
     await conn.sendMessage(m.chat, { contacts: { displayName: 'Juanca⁩', contacts: [{ vcard }] }}, { quoted: m });
 }
 
