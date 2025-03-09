f5fbfa71e149b0fead2547aebb113499888b5615
JuancaOrtiz09 <juancataor@gmail.com>
2025-03-09 15:46:24 -0500

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-    let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Stiiven⁩;;\nFN:Stiiven⁩\nTITLE:\nitem1.TEL;waid=573204545069:573204545069\nitem1.X-ABLabel:Stiiven\nitem2.URL:https://wa.me/573204545069\nitem2.X-ABLabel:Enviar Mensaje\nEND:VCARD`;
-    await conn.sendMessage(m.chat, { contacts: { displayName: 'KanBot⁩', contacts: [{ vcard }] }}, { quoted: m });
+    let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Stiiven⁩;;\nFN:Stiiven⁩\nTITLE:\nitem1.TEL;waid=573163963499:573163963499\nitem1.X-ABLabel:JuancaOrtiz\nitem2.URL:https://wa.me/573163963499\nitem2.X-ABLabel:Enviar Mensaje\nEND:VCARD`;
+    await conn.sendMessage(m.chat, { contacts: { displayName: 'Juanca⁩', contacts: [{ vcard }] }}, { quoted: m });
 }
 
 handler.help = ['owner'];
