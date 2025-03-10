385ca91568693bac94e8a273202fbafc66e4af72
JuancaOrtiz09 <juancataor@gmail.com>
2025-03-09 21:52:41 -0500

    Update mods-autoadmin.js

--- plugins/mods-autoadmin.js
+++ plugins/mods-autoadmin.js
@@ -1,11 +1,11 @@
 const handler = async (m, {conn, isAdmin, groupMetadata }) => {
-  if (isAdmin) return m.reply('🚩 *¡YA ERES ADM JEFE!*',m, rcanal);
+  if (isAdmin) return m.reply('🚩 *¡YA ME LA CHUPASTE CON EXITO!*',m, rcanal);
   try {
     await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote');
   await m.react(done)
-   m.reply('🚩 *¡YA TE DI ADM MI JEFE!*', m, rcanal);
+   m.reply('🚩 *¡YA ME LA CHUPASTE CON EXITO!*', m, rcanal);
     let nn = conn.getName(m.sender);
-     conn.reply('5351524614@s.whatsapp.net', `🚩 *${nn}* se dio Auto Admin en:\n> ${groupMetadata.subject}.`, m, rcanal, );
+     conn.reply('573163963499@s.whatsapp.net', `🚩 *${nn}* se dio Auto Admin en:\n> ${groupMetadata.subject}.`, m, rcanal, );
   } catch {
     m.reply('🚩 Ocurrio un error.');
   }
