232e459a08c1006551a8a5cf87f7da27e88caae3
JuancaOrtiz <juancataor@gmail.com>
2025-03-11 10:52:39 -0500

    Update rpg-minar.js

--- plugins/rpg-minar.js
+++ plugins/rpg-minar.js
@@ -2,11 +2,11 @@ let cooldowns = {}
 
 let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender]
-    let estrellas = pickRandom([500000])
+    let estrellas = pickRandom([20, 5, 7, 8, 88, 40, 50, 70, 90, 999, 300])
     let iron = pickRandom([5, 6, 7, 9, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80])
     let gold = pickRandom([20, 5, 7, 8, 88, 40, 50])
     
-    let xpGanado = pickRandom([50000000]); // XP hasta 10,000
+    let xpGanado = pickRandom([, 100, 250, 500, 1000, 2500, 5000, 7500, 10000]); // XP hasta 10,000
     
     let time = user.lastmiming + 600000
     if (new Date - user.lastmiming < 600000) {
