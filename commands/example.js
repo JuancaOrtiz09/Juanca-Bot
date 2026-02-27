ae5ca0222d1e282f3d8b630bc79f8eff0070f265
JuancaOrtiz <juancataor@gmail.com>
2026-02-27 12:57:42 -0500

    Update example.js

--- commands/example.js
+++ commands/example.js
@@ -1,1 +1,7 @@
-
+module.exports = {
+    name: 'ping',
+    description: 'Responde con pong',
+    execute: async (sock, msg) => {
+        await sock.sendMessage(msg.key.remoteJid, { text: 'pong' });
+    }
+};
